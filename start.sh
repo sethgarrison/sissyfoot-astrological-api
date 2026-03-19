002600554ec80a945009265c424df148c43f118c
Seth Garrison <seth.garrison.-nd@disney.com>
2026-03-19 13:56:23 -0700

    added overwrite on start for testing data

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 set -e
 # Seed reference tables and interpretation data on startup (idempotent; preserves existing content)
 python -m database.seed
-python -m database.seed_from_csv
+python -m database.seed_from_csv --overwrite
 exec uvicorn main:app --host 0.0.0.0 --port 10000
